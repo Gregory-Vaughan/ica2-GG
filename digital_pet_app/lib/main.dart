b7f64e042f2f66f5b9a1fde2fce8d5f20d48109a
Griffin McCue <mccuegriffin3401@gmail.com>
2025-02-05 19:39:21 -0500

    This is my first commit from Griffin.

--- digital_pet_app/lib/main.dart
+++ digital_pet_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// This is my second attempt at first commit.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
